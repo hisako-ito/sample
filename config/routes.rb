7974e477f748b11b16b32756d4d33c82c777ea83
hisako-ito <[hisahisaito@gmail.com]>
2022-04-08 15:57:05 +0000

    CRUD処理途中まで実施

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'tweets/new'
-  get 'tweets/index'
-  get 'tweets/show'
-  get 'tweets/create'
-  get 'users/index'
-  get 'users/show'
+  
+  root 'tweets#index'  # 追加
+  
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :tweets  # 追加
+  resources :users  # 追加
 end
