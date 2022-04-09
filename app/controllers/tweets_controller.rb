c401b6819f0375e6d9aa4bb64b2f329347aa3ca0
hisako-ito <[hisahisaito@gmail.com]>
2022-04-09 12:14:08 +0000

    一対多の実装完成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -10,8 +10,23 @@ class TweetsController < ApplicationController
 
   def show
     @tweet = Tweet.find(params[:id])
+    # ===============追加==============
+    @user = @tweet.user
+# ================================
   end
+  # ===============追加=============
+  private
+    def tweet_params
+      params.require(:tweet).permit(:body) # tweetモデルのカラムのみを許可
+    end
+# =================================
 
   def create
+    # ============追加================
+    @tweet = Tweet.new(tweet_params)  # フォームから送られてきたデータ(body)をストロングパラメータを経由して@tweetに代入
+    @tweet.user_id = current_user.id # user_idの情報はフォームからはきていないので、deviseのメソッドを使って「ログインしている自分のid」を代入
+    @tweet.save
+    redirect_to tweets_path
+# =================================
   end
 end
