be6672a3c5b5f66f663604d6c38211b3bb79d74b
hisako-ito <[hisahisaito@gmail.com]>
2022-04-12 14:32:40 +0000

    インデント修正

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,7 +3,7 @@ class Tweet < ApplicationRecord
   has_many :favorites
   
   def favorited_by?(user)
-        favorites.where(user_id: user.id).exists?
+      favorites.where(user_id: user.id).exists?
     end
     
 end
